c04e5465aff646ae50f6c2dfaeced2cdcd6bf076
Stephen Yang <kwyang@gmail.com>
2022-02-06 18:03:19 +0800

    Update solution.py

--- 05_PrefixSums/MinAvgTwoSlice/solution.py
+++ 05_PrefixSums/MinAvgTwoSlice/solution.py
@@ -1,23 +1,31 @@
-## https://app.codility.com/demo/results/training2MHSNY-VQ5/
+## https://app.codility.com/demo/results/trainingC2F8N2-2TY/
 
 def solution(A):
     # write your code in Python 3.6
-    avg2 = sum(A[0:2])/2
-    avg3 = sum(A[0:3])/3
+    if len(A)==2:
+        return 0
 
-    min_idx = 0
-    min_avg = min(avg2, avg3)
-    
-    for i in range(1, len(A)-2):
-        avg2 = sum(A[i:i+2])/2
-        avg3 = sum(A[i:i+3])/3
-        this_min = min(avg2, avg3)
-        if min_avg>this_min:
-            min_avg = this_min
-            min_idx = i
+    minSlice2 = A[0] + A[1]
+    minIndex2 = 0
 
-    avg2 = sum(A[-2:])/2
-    if min_avg>avg2:
-        return len(A)-2
+    minSlice3 = A[0] + A[1] + A[2]
+    minIndex3 = 0
 
-    return min_idx
+    for i in range(2, len(A)):
+        slice2 = A[i-1] + A[i]
+        if slice2<minSlice2:
+            minSlice2 = slice2
+            minIndex2 = i - 1
+
+        slice3 = A[i-2] + A[i-1] + A[i]
+        if slice3<minSlice3:
+            minSlice3 = slice3
+            minIndex3 = i - 2
+
+    minSlice2 *= 3
+    minSlice3 *= 2
+
+    if minSlice2==minSlice3:
+        return min(minIndex2, minIndex3)
+
+    return minIndex2 if minSlice2<minSlice3 else minIndex3
