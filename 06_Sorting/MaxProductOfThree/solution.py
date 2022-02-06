ca6c30e76bf720cd111631d4ed3388126e725bbd
Stephen Yang <kwyang@gmail.com>
2022-02-06 18:18:23 +0800

    Update solution.py

--- 06_Sorting/MaxProductOfThree/solution.py
+++ 06_Sorting/MaxProductOfThree/solution.py
@@ -1,8 +1,6 @@
-## https://app.codility.com/demo/results/trainingFJN95J-U7Y/
+## https://app.codility.com/demo/results/trainingZKYGE5-VHM/
 
 def solution(A):
     # write your code in Python 3.6
-    A = sorted(A, reverse=True)
-    p3 = lambda x, y, z: x*y*z
-    results = [p3(A[0], A[1], A[2]), p3(A[0], A[-1], A[-2])]
-    return max(results)
+    a = sorted(A, reverse=True)
+    return max(a[0]*a[1]*a[2], a[0]*a[-1]*a[-2])
